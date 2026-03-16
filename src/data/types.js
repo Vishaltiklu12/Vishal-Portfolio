9d30c90d37c1ed63549e1c5d68c4e718943ba0a3
Vishal Tiklu <75404887+Vishaltiklu12@users.noreply.github.com>
2026-03-16 15:13:21 -0700

    Refactor Project type to JSDoc format
    Updated Project type definition to JSDoc format with additional properties.

---
+++ src/data/types.js
@@ -0,0 +1,21 @@
+/**
+ * @typedef {Object} Project
+ * @property {string} id
+ * @property {string} title
+ * @property {string} year
+ * @property {string} domain
+ * @property {string} domainGroup
+ * @property {string} cover
+ * @property {string} tagline
+ * @property {string[]} [bullets]
+ * @property {string} summary
+ * @property {string[]} tech
+ * @property {string} problem
+ * @property {string} architecture
+ * @property {string[]} contributions
+ * @property {string[]} [highlights]
+ * @property {{label: string, href: string}[]} [assets]
+ * @property {{github?: string, live?: string}} [links]
+ */
+
+export {}
